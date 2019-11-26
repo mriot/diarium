69ba6f9a3d9e5feb3a2dce3b421f74abe555e893
Markus Kremer <mkremer@seibert-media.net>
2019-11-26 17:55:33 +0100

    implement codemirror; stuff

--- src/components/editor/markdown-editor.jsx
+++ src/components/editor/markdown-editor.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import Editor from 'for-editor'
+import ReactMarkdown from "react-markdown";
+import SeparatorHandle from "./separator-handle";
+import CodeMirror from "react-codemirror";
+import MarkdownView from './markdown-view';
+import "codemirror/lib/codemirror.css";
+import "./theme/lucario2.css";
+import "codemirror/mode/markdown/markdown";
 
 const ViewContainer = styled.div `
 
@@ -8,26 +16,35 @@ const ViewContainer = styled.div `
 const EditorContainer = styled.div `
 	width: 100%;
   position: relative;
-	background-color: #555;
+	background-color: #fff;
 	display: flex;
 `
-const SeparatorHandle = styled.div `
-	position: absolute;
-	left: 50%;
+const StyledCodeMirror = styled(CodeMirror) `
+	width: 50%;
 	height: 100%;
-	width: 7px;
-	background-color: #777;
-	cursor: col-resize;
 `
 
 export default class MarkdownEditor extends React.PureComponent {
 	constructor(props) {
 		super(props);
 	
+		this.editorContainer = React.createRef();
+		this.markdownView = React.createRef();
+		this.markdownEditor = React.createRef();
+
 		this.state = {
 			markdown: "# Hello World!",
-			viewMode: false
+			viewMode: false,
+			ready: false,
 		}
+	}
+
+	componentDidMount() {
+		this.markdownContainer = ReactDOM.findDOMNode(this.editorContainer.current);
+		this.editorNode = ReactDOM.findDOMNode(this.markdownEditor.current);
+		this.viewNode = ReactDOM.findDOMNode(this.markdownView.current);
+		console.log(this.markdownContainer);
+		this.setState({ready: true})
 	}
 
 	render() {
@@ -39,33 +56,25 @@ export default class MarkdownEditor extends React.PureComponent {
 					</ViewContainer>
 				}
 				{!this.state.viewMode &&
-					<EditorContainer>
-						<Editor
-							style={{
-								"width": "100%",
-								"height": "100%"
-							}}
-							language={"en"}
-							subfield={true}
-							preview={true}
-							toolbar={{
-								h1: false,
-								h2: false,
-								h3: false,
-								h4: false,
-								img: true,
-								link: true,
-								code: true,
-								preview: false,
-								expand: true,
-								undo: true,
-								redo: true,
-								save: true,
-								subfield: true
-							}}
-							value={this.state.markdown}
-							onChange={value => this.setState({markdown: value})}
-						/>
+					<EditorContainer ref={this.editorContainer}>
+						<StyledCodeMirror ref={this.markdownEditor} value={this.state.markdown} onChange={(markdown, ...args) => {
+							// console.log(markdown, ...args);
+							this.setState({markdown})
+						}} options={{
+							mode: "markdown",
+							lineNumbers: true,
+							theme: "lucario"
+						}} />
+
+						{this.state.ready &&
+							<SeparatorHandle
+								containerNode={this.markdownContainer}
+								editorNode={this.editorNode}
+								viewNode={this.viewNode}
+							/>
+						}
+
+						<MarkdownView markdown={this.state.markdown} ref={this.markdownView} />
 					</EditorContainer>
 				}
 			</>
