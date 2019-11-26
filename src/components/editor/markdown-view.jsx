538dfad4ab27830704e3fa8280ae50b5fda749f5
Markus Kremer <mkremer@seibert-media.net>
2019-11-26 17:55:59 +0100

    add dedicated view for markdown

---
+++ src/components/editor/markdown-view.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactMarkdown from "react-markdown";
+import "./theme/ceres.css";
+
+const RenderedMarkdownContainer = styled.div `
+	width: 50%;
+	padding: 0 5px;
+	box-sizing: border-box;
+`
+
+export default class MarkdownView extends React.PureComponent {
+	render() {
+		return (
+			<RenderedMarkdownContainer className="markdown-body">
+				<ReactMarkdown source={this.props.markdown} />
+			</RenderedMarkdownContainer>
+		);
+	}
+}
