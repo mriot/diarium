23bec2bd68162e48b78bbc78f475608dc20d7b0c
Markus Kremer <mkremer@seibert-media.net>
2019-11-30 22:50:38 +0100

    disable console log

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -62,7 +62,7 @@ export default class Sidebar extends React.PureComponent {
           tileClassName = {
             ({activeStartDate, date, view}) => {
               if (view === "month") {
-                console.log(date.getDate(), date.getMonth() + 1, date.getFullYear());
+                // console.log(date.getDate(), date.getMonth() + 1, date.getFullYear());
                 // TODO: :)
                 if (date.getDate() === 2) {
                   return "marked"
