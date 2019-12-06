80d85ee2247d8b66cce98a3ac8cae995d1a2a69f
Markus Kremer <mkremer@seibert-media.net>
2019-12-06 13:28:01 +0100

    extract calendar from sidebar into its own component

---
+++ src/components/sidebar/diarium-calendar.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import styled from "styled-components";
+import Calendar from 'react-calendar';
+import "./calendar-theme.css";
+
+const StyledCalendar = styled(Calendar) `
+  border-bottom: 1px solid #191919;
+	box-shadow: 0px 2px 3px 0 rgba(0, 0, 0, 0.2);
+`
+
+export default class DiariumCalendar extends React.PureComponent {
+	constructor(props) {
+		super(props);
+	
+		this.state = {
+			calendarInitDate: new Date(),
+		}
+	}
+
+	// async getHolidays() {
+  //   return fetch(`https://feiertage-api.de/api/?jahr=2019&nur_land=HE`, {
+  //     method: "GET",
+  //   }).then(response => response.json())
+	// }
+	
+	componentDidMount() {
+		// this.holidays = [];
+    // this.getHolidays().then(response => {
+    //   for (const key in response) {
+    //     this.holidays.push({[response[key].datum]: key})
+    //   }
+    //   console.log(this.holidays)
+    // })
+	}
+	
+
+	render() {
+		return (
+			<StyledCalendar
+				className="calendar-dark-theme"
+				key={this.props.forceUpdateCalendar}
+				value={this.state.calendarInitDate}
+				minDetail={"decade"}
+				minDate={new Date(2019, 0, 1)}
+				tileClassName = {
+					({activeStartDate, date, view}) => {
+						if (view === "month") {
+							// console.log(date.getDate(), date.getMonth() + 1, date.getFullYear());
+							// if (date.getDate() === 2) {
+							//   return "marked"
+							// }
+
+							// EXAMPLE DATA
+							switch (date.getDate()) {
+								case 2:
+									return "marked holiday"
+								case 4:
+									return "marked"
+								case 10:
+									return "vacation"
+								case 11:
+								return "vacation"
+								case 12:
+									return "vacation"
+								case 14:
+									return "sick"
+								case 23:
+									return "sick"
+								case 24:
+									return "sick"
+								case 25:
+									return "sick"
+								default:
+									return ""
+							}
+						}
+					}
+				}
+
+				// arrow navigation (view = month => fetch overview)
+				onActiveDateChange={(...args) => console.log("onActiveDateChange", ...args)}
+				// month selector (fetch overview)
+				onClickMonth={(...args) => console.log("onClickMonth:", ...args)}
+				// date/day changed (fetch selected day -> all data)
+				onClickDay={(...args) => console.log("onClickDay", ...args)}
+				
+				// > year selector (don't need fetch imo)
+				// onClickYear={(...args) => console.log("onClickYear", ...args)}
+
+				// > general listener
+				// onChange={value => console.log("onChange:", value)}
+			/>
+		);
+	}
+}
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
-import Calendar from 'react-calendar';
 import Progress from './progress';
-import FetchActions from './fetchactions';
+import DiariumCalendar from './diarium-calendar';
 
 const StyledSidebar = styled.aside`
   position: relative;
@@ -22,37 +21,18 @@ const Today = styled.div `
     text-decoration: underline;
   }
 `
-const StyledCalendar = styled(Calendar) `
-  border-bottom: 1px solid #191919;
-	box-shadow: 0px 2px 3px 0 rgba(0, 0, 0, 0.2);
-`
 
 export default class Sidebar extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      calendarInitDate: new Date(),
       dateToday: new Date(),
       forceUpdateCalendar: 0,
     }
   }
 
-  // async getHolidays() {
-  //   return fetch(`https://feiertage-api.de/api/?jahr=2019&nur_land=HE`, {
-  //     method: "GET",
-  //   }).then(response => response.json())
-  // }
-
   componentDidMount() {
-    // this.holidays = [];
-    // this.getHolidays().then(response => {
-    //   for (const key in response) {
-    //     this.holidays.push({[response[key].datum]: key})
-    //   }
-    //   console.log(this.holidays)
-    // })
-
     setInterval(this.updateTodaysDate.bind(this), 1000 * 60 * 5) // 5 minutes
   }
 
@@ -79,60 +59,10 @@ export default class Sidebar extends React.PureComponent {
             })
           }
         </Today>
-        <StyledCalendar
-          className="calendar-dark-theme"
-          key={this.state.forceUpdateCalendar}
-          value={this.state.calendarInitDate}
-          minDetail={"decade"}
-          minDate={new Date(2019, 0, 1)}
-          tileClassName = {
-            ({activeStartDate, date, view}) => {
-              if (view === "month") {
-                // console.log(date.getDate(), date.getMonth() + 1, date.getFullYear());
-                // if (date.getDate() === 2) {
-                //   return "marked"
-                // }
-                switch (date.getDate()) {
-                  case 2:
-                    return "marked holiday"
-                  case 4:
-                    return "marked"
-                  case 10:
-                    return "vacation"
-                  case 11:
-                  return "vacation"
-                  case 12:
-                    return "vacation"
-                  case 14:
-                    return "sick"
-                  case 23:
-                    return "sick"
-                  case 24:
-                    return "sick"
-                  case 25:
-                    return "sick"
-                  default:
-                    return ""
-                }
-              }
-            }
-          }
 
-          // arrow navigation (view = month => fetch)
-          onActiveDateChange={(...args) => console.log("onActiveDateChange", ...args)}
-          // month selector (fetch)
-          onClickMonth={(...args) => console.log("onClickMonth:", ...args)}
+        <DiariumCalendar forceUpdateCalendar={this.state.forceUpdateCalendar} />
 
-          // date/day changed  (fetch? -> maybe refetch day)
-          onClickDay={(...args) => console.log("onClickDay", ...args)}
-          
-          // > year selector (don't need fetch imo)
-          // onClickYear={(...args) => console.log("onClickYear", ...args)}
-          // > general listener
-          // onChange={value => console.log("onChange:", value)}
-        />
         <Progress />
-        {/* <FetchActions /> */}
       </StyledSidebar>
     );
   }
