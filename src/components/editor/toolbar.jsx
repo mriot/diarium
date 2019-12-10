e0b0c0fd5bd11cf181e534533eec513ecde6633a
Markus Kremer <mkremer@seibert-media.net>
2019-12-10 20:39:55 +0100

    add/improve animations; readmode stuff (wip)

--- src/components/editor/toolbar.jsx
+++ src/components/editor/toolbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import styled from 'styled-components';
-import uuid4 from "uuid/v4";
 import { AnimateKeyframes } from "react-simple-animate";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUndoAlt, faColumns, faImage, faLink, faCode, faExpand, faCompress, faShare, faReply, faVihara, faPaperclip, faArrowsAltH, faAnchor } from '@fortawesome/free-solid-svg-icons';
@@ -20,7 +19,6 @@ const StyledToolbar = styled.div `
 	border-top-right-radius: 8px;
 	
 	${props => props.toolbarStatus.zenModeActive && `
-		height: 35px;
 		border-radius: 0;
 		border-bottom: none;
 	`}
@@ -60,17 +58,6 @@ const IconButton = styled.div `
 		color: #777;
 		pointer-events: none;
 	`}
-
-/*
-	&::before {
-		content: "${props => props.displayCounter > 0 && props.displayCounter}";
-		position: absolute;
-		top: 0;
-		right: 0;
-		font-size: 11px;
-		opacity: 0.5;
-	}
-*/
 `
 
 export default class Toolbar extends React.PureComponent {
@@ -79,7 +66,7 @@ export default class Toolbar extends React.PureComponent {
 	
 		this.state = {
 			inFullscreenMode: false,
-			visible: true,
+			readMode: this.props.toolbarStatus.readModeActive,
 		}
 		
 		this.toolbarItems = {
@@ -87,35 +74,74 @@ export default class Toolbar extends React.PureComponent {
 				{
 					title: "Rückgängig",
 					onClick: () => this.props.editorUndo(),
-					isDisabled: this.props.toolbarStatus.editorHistory.undo <= 0,
+					isDisabled: () => this.props.toolbarStatus.editorHistory.undo <= 0,
 					icon: faReply,
-				}, {
+				},
+				{
 					title: "Wiederholen",
 					onClick: () => this.props.editorRedo(),
-					isDisabled: this.props.toolbarStatus.editorHistory.redo <= 0,
+					isDisabled: () => this.props.toolbarStatus.editorHistory.redo <= 0,
 					icon: faShare,
-				}, {
+				},
+				{
 					title: "Bild einfügen",
 					onClick: null,
 					icon: faImage,
-				}, {
+				},
+				{
 					title: "Link einfügen",
 					onClick: () => this.props.insertLink(),
 					icon: faLink,
-				}, {
+				},
+				{
 					title: "Code einfügen",
 					onClick: () => this.props.insertCode(),
 					icon: faCode,
-				}, {
+				},
+				{
 					separator: true,
-				}, {
+				},
+				{
 					title: "Scrollen synchronisieren",
 					onClick: () => this.props.toggleScrollSync(),
-					isActive: this.props.toolbarStatus.scrollSyncActive,
+					isActive: () => this.props.toolbarStatus.scrollSyncActive,
 					icon: faArrowsAltH
 				},
 			],
-			right: []
+			
+			right: [
+				{
+					title: "Zen-Mode",
+					onClick: () => this.props.toggleZenMode(),
+					isActive: () => this.props.toolbarStatus.zenModeActive,
+					icon: faVihara,
+				},
+				{
+					title: "Ansicht splitten",
+					onClick: () => this.props.togglePreview(),
+					isActive: () => this.props.toolbarStatus.previewActive,
+					icon: faColumns,
+					hiddenInReadMode: true,
+				},
+				{
+					title: "Vollbild",
+					onClick: () => {
+						this.toggleFullScreen();
+						this.props.editorFocus();
+					},
+					isActive: () => this.state.inFullscreenMode,
+					icon: this.state.inFullscreenMode ? faCompress : faExpand,
+				},
+				{
+					title: "Layout des Editors zurücksetzen",
+					onClick: () => {
+						this.props.resetEditorLayout();
+						this.state.inFullscreenMode && this.toggleFullScreen();
+					},
+					icon: faUndoAlt,
+					hiddenInReadMode: true,
+				},
+			],
 		}
 	}
 
@@ -128,6 +154,10 @@ export default class Toolbar extends React.PureComponent {
 				this.setState({inFullscreenMode: false});
 			}
 		})
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		this.setState({readMode: this.props.toolbarStatus.readModeActive})
 	}
 	
 	toggleFullScreen() {
@@ -151,79 +181,60 @@ export default class Toolbar extends React.PureComponent {
 					{this.toolbarItems.left.map((item, index) => 
 						<AnimateKeyframes
 							key={index}
-							play={this.state.visible}
+							play={this.state.readMode}
 							duration={this.animationDuration || 0}
 							iterationCount={1}
 							fillMode="forwards"
-							direction={this.state.visible ? "normal" : "reverse"}
+							direction={this.state.readMode ? "normal" : "reverse"}
 							keyframes={[
-								{0: "transform: scale(0)"},
+								{0: "transform: scale(1)"},
 								{50: "transform: scale(1.1)"},
-								{100: "transform: scale(1)"},
+								{100: "transform: scale(0)"},
 							]}
 							render={({ style }) => ([
 								!item.separator && 
 									<IconButton
+										key={index}
 										style={style}
-										key={uuid4()}
 										title={item.title}
 										onClick={item.onClick}
-										isActive={item.isActive}
-										isDisabled={item.isDisabled}
+										isActive={item.isActive && item.isActive()}
+										isDisabled={item.isDisabled && item.isDisabled()}
 									>
 										<FontAwesomeIcon icon={item.icon} />
 									</IconButton>,
 
-								item.separator && <ButtonSeparator style={style} />
+								item.separator && <ButtonSeparator style={style} key={index} />
 							])}
 						/>
 					)}
 				</LeftSide>
 
 				<RightSide>
-				<IconButton
-						title="TOGGELELE"
-						onClick={() => this.setState({visible: !this.state.visible})}
-					>
-						<FontAwesomeIcon icon={faAnchor} />
-					</IconButton>
-
-					<IconButton
-						title="Zen-Mode"
-						onClick={() => this.props.toggleZenMode()}
-						isActive={this.props.toolbarStatus.zenModeActive}
-					>
-						<FontAwesomeIcon icon={faVihara} />
-					</IconButton>
-
-					<IconButton
-						title="Ansicht splitten"
-						onClick={() => this.props.togglePreview()}
-						isActive={this.props.toolbarStatus.previewActive}
-					>
-						<FontAwesomeIcon icon={faColumns} />
-					</IconButton>
-
-					<IconButton
-						title="Vollbild"
-						onClick={() => {
-							this.toggleFullScreen();
-							this.props.editorFocus();
-						}}
-						isActive={this.state.inFullscreenMode}
-					>
-						<FontAwesomeIcon icon={this.state.inFullscreenMode ? faCompress : faExpand} />
-					</IconButton>
-
-					<IconButton
-						title="Layout des Editors zurücksetzen"
-						onClick={() => {
-							this.props.resetEditorLayout();
-							this.state.inFullscreenMode && this.toggleFullScreen();
-						}}
-					>
-						<FontAwesomeIcon icon={faUndoAlt} />
-					</IconButton>
+					{this.toolbarItems.right.map((item, index) => 
+						<AnimateKeyframes
+							key={index}
+							play={this.state.readMode}
+							duration={this.animationDuration || 0}
+							iterationCount={1}
+							fillMode="forwards"
+							direction={this.state.readMode ? "normal" : "reverse"}
+							keyframes={[
+								{100: "width: 0; padding: 0; margin: 0; overflow: hidden;"},
+							]}
+							render={({ style }) => (
+								<IconButton
+									key={index}
+									style={item.hiddenInReadMode && style}
+									title={item.title}
+									onClick={item.onClick}
+									isActive={item.isActive && item.isActive()}
+								>
+									<FontAwesomeIcon icon={item.icon} />
+								</IconButton>
+							)}
+						/>
+					)}
 				</RightSide>
 			</StyledToolbar>
 		);
