d36d7a4cf07cb3778496acd2621e721f5c7b0d10
Markus Kremer <mkremer@seibert-media.net>
2019-12-18 20:54:50 +0100

    move favorrites into own folder

---
+++ src/components/favorites/favorites.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import posed from 'react-pose';
+import { favoritesAnimation } from "./animations";
+import FavoriteCard from './favorite-card';
+import moment from 'moment';
+
+const PosedFavoritesContainer = posed.div(favoritesAnimation);
+const FavoritesContainer = styled(PosedFavoritesContainer) `
+	width: 100%;
+	/* height: 100%; */
+	color: #fff;
+	position: relative;
+	z-index: 1; /* place on top of editor */
+	display: flex;
+	flex-wrap: wrap;
+	background-color: #191919;
+	transform-origin: center;
+	backface-visibility: hidden;
+`
+
+export default class Favorites extends React.PureComponent {
+	render() {
+		return (
+			<FavoritesContainer {...this.props}>
+				<FavoriteCard date={moment()} tags={["lorem", "ipsum"]} desc={"My first favorite!"} />
+				<FavoriteCard date={moment()} tags={["lorem", "ipsum"]} desc={"My first favorite!"} />
+				<FavoriteCard date={moment()} tags={["lorem", "ipsum"]} desc={"My first favorite!"} />
+				<FavoriteCard date={moment()} tags={["lorem", "ipsum"]} desc={"My first favorite!"} />
+				<FavoriteCard date={moment()} tags={["lorem", "ipsum"]} desc={"My first favorite!"} />
+				<FavoriteCard date={moment()} tags={["lorem", "ipsum"]} desc={"My first favorite!"} />
+			</FavoritesContainer>
+		);
+	}
+}
+
+Favorites.propTypes = {
+	_pose: PropTypes.any,
+
+}
