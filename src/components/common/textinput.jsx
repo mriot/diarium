b157dee6787281ac40a8c22211e98c2cd9da3693
Markus Kremer <mkremer@seibert-media.net>
2020-01-03 00:38:53 +0100

    redesign textinput

--- src/components/common/textinput.jsx
+++ src/components/common/textinput.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const StyledTextInput = styled.input `
-  color: #9e9e9e;
+  color: ${props => (props.light ? "#000" : "#9e9e9e")};
   outline: none;
   border: none;
   border-radius: 3px;
@@ -10,7 +10,7 @@ const StyledTextInput = styled.input `
   margin-top: 0;
   padding: 7px 14px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  background-color: rgba(100, 100, 100, 0.5);
+  background-color: ${props => (props.light ? "#eee" : "rgba(100, 100, 100, 0.5)")};
   transition: border 0.3s, box-shadow 0.2s, color 0.5s;
 
   &::placeholder {
@@ -18,25 +18,25 @@ const StyledTextInput = styled.input `
   }
   
   &:focus {
-    box-shadow: inset 0 0 0 1px #555;
+    box-shadow: 0 0 0 1px #555;
   }
 
   &:disabled {
     opacity: 0.65;
     pointer-events: none;
   }
-`
+`;
 
 export default class TextInput extends React.PureComponent {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.textInputRef = React.createRef();
-  }
+		this.textInputRef = React.createRef();
+	}
 
-  render() {
-    return (
-      <StyledTextInput {...this.props} ref={this.textInputRef} />
-    );
-  }
+	render() {
+		return (
+			<StyledTextInput {...this.props} ref={this.textInputRef} />
+		);
+	}
 }
