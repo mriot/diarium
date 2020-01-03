d68a6a3b11faafeb73f4abd4fccc676f183ace13
Markus Kremer <mkremer@seibert-media.net>
2020-01-03 01:37:48 +0100

    implement basic auth

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -1,10 +1,34 @@
-import { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-// GLOBALS
+// GLOBALS ====================================================
 const BACKEND_URL = "http://localhost:5000/api";
-const GLOBAL_TOKEN = null;
+let GLOBAL_TOKEN = null;
 
 
+// LOGIN / AUTH ====================================================
+export const auth = (username, password) => {
+	return fetch(`${BACKEND_URL}/auth`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ username, password })
+	}).then(response => {
+		if (!response.ok) {
+			throw new Error(`${response.status} (${response.statusText})`);
+		}
+		return response.json();
+	})
+		.then(response => {
+			console.log(jwt.decode(response.token, { complete: true }));
+
+			localStorage.setItem("token", response.token);
+			GLOBAL_TOKEN = response.token;
+			return response;
+		});
+};
+
+// COUNT ALL ====================================================
 export const countAllEntries = () => {
 	return fetch(`${BACKEND_URL}/entries/count/`, {
 		method: "GET"
