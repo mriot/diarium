d4f5bd0ceac731227d9164574e7f77e2f6e63449
Markus Kremer <mkremer@seibert-media.net>
2020-01-03 14:47:17 +0100

    add login check, some endpoints, ...

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import moment from "moment";
 
 // GLOBALS ====================================================
 const BACKEND_URL = "http://localhost:5000/api";
@@ -20,18 +21,54 @@ export const auth = (username, password) => {
 		return response.json();
 	})
 		.then(response => {
-			console.log(jwt.decode(response.token, { complete: true }));
-
+			// console.log(jwt.decode(response.token, { complete: true }));
 			localStorage.setItem("token", response.token);
 			GLOBAL_TOKEN = response.token;
 			return response;
 		});
 };
 
+export const isLoggedIn = () => {
+	const token = localStorage.getItem("token");
+	const decodedToken = jwt.decode(token);
+	if (!token || !decodedToken) return false;
+	if (!decodedToken.exp || decodedToken.exp <= moment().unix()) {
+		localStorage.removeItem("token");
+		return false;
+	}
+	GLOBAL_TOKEN = token;
+	return true;
+};
+
+
 // COUNT ALL ====================================================
 export const countAllEntries = () => {
 	return fetch(`${BACKEND_URL}/entries/count/`, {
 		method: "GET"
+	})
+		.then(response => response.json());
+};
+
+
+// GET ====================================================
+export const getAllEntriesForYear = year => {
+	return fetch(`${BACKEND_URL}/entries/${year}/`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${GLOBAL_TOKEN}`
+		},
+	})
+		.then(response => response.json());
+};
+
+export const getAllEntriesForYearMonth = (year, month) => {
+	return fetch(`${BACKEND_URL}/entries/${year}/${month}/`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${GLOBAL_TOKEN}`
+		},
 	})
 		.then(response => response.json());
 };
