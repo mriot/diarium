8106ec88b384c6a7553be2c9cc1ed61e96fc9bba
Markus Kremer <mkremer@seibert-media.net>
2020-01-06 13:59:41 +0100

    add two more endpoint methods

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -54,6 +54,20 @@ export const countAllEntries = () => {
 
 
 // GET ====================================================
+export const getRecordsInRange = (start, end, columns) => {
+	const params = (columns.map(col => `column=${col}`).join("&"));
+
+	return fetch(`${BACKEND_URL}/entries/range/?start=${start}&end=${end}&${params}`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${GLOBAL_TOKEN}`
+		},
+	})
+		.then(response => response.json())
+		.catch(error => console.error(error));
+};
+
 export const getAllEntriesForYear = year => {
 	return fetch(`${BACKEND_URL}/entries/${year}/`, {
 		method: "GET",
@@ -68,6 +82,18 @@ export const getAllEntriesForYear = year => {
 
 export const getAllEntriesForYearMonth = (year, month) => {
 	return fetch(`${BACKEND_URL}/entries/${year}/${month}/`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${GLOBAL_TOKEN}`
+		},
+	})
+		.then(response => response.json())
+		.catch(error => console.error(error));
+};
+
+export const getRecordForDay = (year, month, day) => {
+	return fetch(`${BACKEND_URL}/entries/${year}/${month}/${day}`, {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
