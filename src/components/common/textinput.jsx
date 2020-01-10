40204d191ee0a365e9d9007ba1bbe8f5b20857f7
Markus Kremer <mkremer@seibert-media.net>
2020-01-10 22:37:46 +0100

    update textinput comp

--- src/components/common/textinput.jsx
+++ src/components/common/textinput.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledTextInput = styled.input `
+  appearance: none;
+  width: 55%;
   color: ${props => (props.light ? "#000" : "#9e9e9e")};
   outline: none;
   border: none;
@@ -9,6 +11,7 @@ const StyledTextInput = styled.input `
   font-size: inherit;
   margin-top: 0;
   padding: 7px 14px;
+  box-sizing: border-box;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   background-color: ${props => (props.light ? "#eee" : "rgba(100, 100, 100, 0.5)")};
   transition: border 0.3s, box-shadow 0.2s, color 0.5s;
