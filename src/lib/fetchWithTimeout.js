2b686e01e3314b160019fd7e0b72a450fff545a2
Markus Kremer <mkremer@seibert-media.net>
2020-01-14 21:58:24 +0100

    still wip

--- src/lib/fetchWithTimeout.js
+++ src/lib/fetchWithTimeout.js
@@ -1,7 +1,11 @@
 const controller = new AbortController();
 const { signal } = controller;
 
-export default (url, options, timeout = 5000) => {
-	setTimeout(() => controller.abort(), timeout);
-	return fetch(url, { ...options, signal });
+export const fetch2 = (url, options, timeout = 5000) => {
+	const to = setTimeout(() => controller.abort(), timeout);
+	return fetch(url, { ...options, signal })
+		.then(res => {
+			clearTimeout(to);
+			return res;
+		});
 };
