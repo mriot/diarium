74d95f57cbca59f49f08afcd9547a8a9a466fd0a
Markus Kremer <mkremer@seibert-media.net>
2020-01-16 20:03:56 +0100

    add transition effect & prop types; eslint compliant

--- src/components/common/progressbar.jsx
+++ src/components/common/progressbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const ProgressContainer = styled.div `
@@ -8,15 +9,15 @@ const ProgressContainer = styled.div `
 	position: relative;
 	overflow: hidden;
 	border-radius: 10px;
-`
+`;
 
 const ProgressFilling = styled.div `
 	position: absolute;
 	width: ${props => props.progress}%;
 	height: 100%;
-	/* background-color: #00ff8b; */
 	background-color: #00b7ff;
-`
+	transition: all 500ms;
+`;
 
 export default class ProgressBar extends React.PureComponent {
 	render() {
@@ -27,3 +28,7 @@ export default class ProgressBar extends React.PureComponent {
 		);
 	}
 }
+
+ProgressBar.propTypes = {
+	progress: PropTypes.number.isRequired,
+};
