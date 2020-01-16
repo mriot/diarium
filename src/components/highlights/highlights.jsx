0fca2d30e199635561727b3c6139c9edbe318353
Markus Kremer <mkremer@seibert-media.net>
2020-01-16 21:29:13 +0100

    eslint compliant

--- src/components/highlights/highlights.jsx
+++ src/components/highlights/highlights.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import posed from 'react-pose';
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import posed from "react-pose";
+import moment from "moment";
 import { highlightsAnimation } from "./animations";
-import HighlightCard from './highlight-card';
-import moment from 'moment';
-import YearAccordion from './YearAccordion';
+import HighlightCard from "./highlight-card";
+import YearAccordion from "./YearAccordion";
 
 const PosedHighlightsContainer = posed.div(highlightsAnimation);
 const HighlightsContainer = styled(PosedHighlightsContainer) `
@@ -22,81 +22,80 @@ const HighlightsContainer = styled(PosedHighlightsContainer) `
 	background-color: #191919;
 	transform-origin: center;
 	backface-visibility: hidden;
-`
+`;
 
-export default class Highlights extends React.PureComponent {	
+export default class Highlights extends React.PureComponent {
 	render() {
 		const cards = [
 			{
 				date: moment("2010-12-12"),
 				desc: "Hello World",
-				tags: ["lorem", "ipsum"]
+				tags: ["lorem", "ipsum"],
 			},
 			{
 				date: moment("2010-11-11"),
 				desc: "Hello World 2",
-				tags: ["lorem", "ipsum"]
+				tags: ["lorem", "ipsum"],
 			},
 			{
 				date: moment("2010-08-08"),
 				desc: "Hello World 3",
-				tags: ["lorem", "ipsum"]
+				tags: ["lorem", "ipsum"],
 			},
 			{
 				date: moment("2010-01-01"),
-				tags: ["lorem", "ipsum"]
+				tags: ["lorem", "ipsum"],
 			},
 		];
 
 		return (
 			<HighlightsContainer {...this.props}>
 				<YearAccordion year={2021} render={accordionOpen => (
-					cards.map((item, index) => 
+					cards.map((item, index) => (
 						<HighlightCard
 							key={index}
 							index={index}
 							date={item.date}
-							tags={item.tags} 
+							tags={item.tags}
 							desc={item.desc}
 							pose={accordionOpen ? "show" : "hide"}
 						/>
-					)
-				)}>
-				</YearAccordion>
+					))
+				)}
+				/>
 
 				<YearAccordion year={2020} render={accordionOpen => (
-					cards.map((item, index) => 
+					cards.map((item, index) => (
 						<HighlightCard
 							key={index}
 							index={index}
 							date={item.date}
-							tags={item.tags} 
+							tags={item.tags}
 							desc={item.desc}
 							pose={accordionOpen ? "show" : "hide"}
 						/>
-					)
-				)}>
-				</YearAccordion>
+					))
+				)}
+				/>
 
 				<YearAccordion year={2019} render={accordionOpen => (
-					cards.map((item, index) => 
+					cards.map((item, index) => (
 						<HighlightCard
 							key={index}
 							index={index}
 							date={item.date}
-							tags={item.tags} 
+							tags={item.tags}
 							desc={item.desc}
 							pose={accordionOpen ? "show" : "hide"}
 						/>
-					)
-				)}>
-				</YearAccordion>
+					))
+				)}
+				/>
 			</HighlightsContainer>
 		);
 	}
 }
 
 Highlights.propTypes = {
-	_pose: PropTypes.any,
-	
-}
+	_pose: PropTypes.any.isRequired,
+};
