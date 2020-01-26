d74bfe834fee85274b1bd3555ded1eaaac9e95a9
Markus Kremer <mkremer@seibert-media.net>
2020-01-26 16:47:24 +0100

    add emoji mehtod call to editor

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -216,6 +216,7 @@ export default class Editor extends React.PureComponent {
 					editorRedo={() => this.markdownEditorRef.editorRedo()}
 					insertCode={() => this.markdownEditorRef.insertCode()}
 					insertLink={() => this.markdownEditorRef.insertLink()}
+					insertEmoji={() => this.markdownEditorRef.insertEmoji()}
 					// called directly here
 					toggleZenMode={() => this.toggleZenMode()}
 					togglePreview={preview ? () => this.hidePreview() : () => this.showPreview()}
