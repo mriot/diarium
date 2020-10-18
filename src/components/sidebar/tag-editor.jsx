897f1c62316e7a32afe38b74c21a011617bb1705
Markus Kremer <mkremer@seibert-media.net>
2020-10-18 17:48:18 +0200

    eslint

--- src/components/sidebar/tag-editor.jsx
+++ src/components/sidebar/tag-editor.jsx
@@ -9,7 +9,7 @@ import { updateExistingEntryById } from "../../lib/backend";
 import { GlobalContext } from "../../contexts";
 import { WHITE } from "../../themes/diarium-theme";
 
-const rotate = keyframes `
+const rotate = keyframes`
 	from {
 		transform: rotate(0)
 	}
@@ -18,7 +18,7 @@ const rotate = keyframes `
 	}
 `;
 
-const TagEditorContainer = styled.div `
+const TagEditorContainer = styled.div`
 	color: ${WHITE};
 	width: 100%;
 	padding: 10px 0;
@@ -26,100 +26,100 @@ const TagEditorContainer = styled.div `
 	border-bottom: 1px solid #191919;
 	box-shadow: 0px 2px 3px 0 rgba(0, 0, 0, 0.2);
 `;
-const Heading = styled.h3 `
+const Heading = styled.h3`
 	margin: 0 0 10px;
 	padding-left: 5px;
 	padding-bottom: 10px;
 	border-bottom: 1px solid #191919;
 `;
-const Spinner = styled(FontAwesomeIcon) `
+const Spinner = styled(FontAwesomeIcon)`
 	opacity: ${props => (props.spinning ? 1 : 0)};
 	font-size: 16px;
 	margin-left: 5px;
 	transition-delay: 100ms;
 	animation: 1s ${rotate} linear infinite;
 `;
-const TagContainer = styled.div ` 
+const TagContainer = styled.div` 
 	display: grid;
 	grid-gap: 3px;
 	grid-template-columns: repeat(2, 1fr);
 `;
 
 export default class TagEditor extends React.PureComponent {
-	constructor(props) {
-		super(props);
-	
-		this.state = {
-			definedTags: {
-				highlight: { label: "Highlight", icon: faTheaterMasks },
-				vacation: { label: "Urlaub", icon: faMapMarkerAlt },
-				sick: { label: "Krank", icon: faBiohazard },
-				nsfw: { label: "NSFW", icon: faLock },
-				rip: { label: "RIP", icon: faCross },
-			},
-			checkboxDisabled: true,
-			spinnerActive: false,
-		};
-	}
+  constructor(props) {
+    super(props);
 
-	componentDidUpdate(prevProps, prevState) {
-		this.setState({ checkboxDisabled: this.context.GLOBAL_READMODE });
-	}
+    this.state = {
+      definedTags: {
+        highlight: { label: "Highlight", icon: faTheaterMasks },
+        vacation: { label: "Urlaub", icon: faMapMarkerAlt },
+        sick: { label: "Krank", icon: faBiohazard },
+        nsfw: { label: "NSFW", icon: faLock },
+        rip: { label: "RIP", icon: faCross }
+      },
+      checkboxDisabled: true,
+      spinnerActive: false
+    };
+  }
 
-	_updateSelectedTags(newSelectedTags) {
-		updateExistingEntryById(this.context.GLOBAL_DAYRECORD.id, { tags: newSelectedTags })
-			.then(result => {
-				if (!result.error) {
-					this.context.UPDATE_GLOBAL_DAYRECORD(result);
-				} else {
-					toast.error("Die Tags konnten leider nicht geupdated werden... 😟");
-				}
-				this.setState({ spinnerActive: false });
-			})
-			.catch(error => console.log(error));
-	}
+  componentDidUpdate(prevProps, prevState) {
+    this.setState({ checkboxDisabled: this.context.GLOBAL_READMODE });
+  }
 
-	addToSelectedTags(tag) {
-		if (this.state.spinnerActive) return;
-		this.setState({ spinnerActive: true });
+  _updateSelectedTags(newSelectedTags) {
+    updateExistingEntryById(this.context.GLOBAL_DAYRECORD.id, { tags: newSelectedTags })
+      .then(result => {
+        if (!result.error) {
+          this.context.UPDATE_GLOBAL_DAYRECORD(result);
+        } else {
+          toast.error("Die Tags konnten leider nicht geupdated werden... 😟");
+        }
+        this.setState({ spinnerActive: false });
+      })
+      .catch(error => console.log(error));
+  }
 
-		this._updateSelectedTags([...this.context.GLOBAL_DAYRECORD.tags, tag]);
-	}
+  addToSelectedTags(tag) {
+    if (this.state.spinnerActive) return;
+    this.setState({ spinnerActive: true });
 
-	removeFromSelectedTags(tag) {
-		if (this.state.spinnerActive) return;
-		this.setState({ spinnerActive: true });
+    this._updateSelectedTags([...this.context.GLOBAL_DAYRECORD.tags, tag]);
+  }
 
-		this._updateSelectedTags(this.context.GLOBAL_DAYRECORD.tags.filter(oldTag => oldTag !== tag));
-	}
+  removeFromSelectedTags(tag) {
+    if (this.state.spinnerActive) return;
+    this.setState({ spinnerActive: true });
 
-	render() {
-		const { definedTags } = this.state;
-		const { GLOBAL_DAYRECORD } = this.context;
+    this._updateSelectedTags(this.context.GLOBAL_DAYRECORD.tags.filter(oldTag => oldTag !== tag));
+  }
 
-		const selectedTags = GLOBAL_DAYRECORD ? GLOBAL_DAYRECORD.tags : [];
-		return (
-			<TagEditorContainer>
-				<Heading>
+  render() {
+    const { definedTags } = this.state;
+    const { GLOBAL_DAYRECORD } = this.context;
+
+    const selectedTags = GLOBAL_DAYRECORD ? GLOBAL_DAYRECORD.tags : [];
+    return (
+      <TagEditorContainer>
+        <Heading>
 					Tags <Spinner icon={faSyncAlt} spinning={this.state.spinnerActive ? 1 : 0} />
-				</Heading>
-				<TagContainer>
-					{Object.keys(definedTags).map((tag, index) => (
-						<Tag
-							key={index}
-							value={tag}
-							label={definedTags[tag].label}
-							icon={definedTags[tag].icon}
-							disabled={this.state.checkboxDisabled}
-							defaultChecked={selectedTags ? selectedTags.some(sTag => sTag === tag) : false}
-							addToSelectedTags={newTag => this.addToSelectedTags(newTag)}
-							removeFromSelectedTags={oldTag => this.removeFromSelectedTags(oldTag)}
-						/>
-					))}
-				</TagContainer>
-			</TagEditorContainer>
-		);
-	}
+        </Heading>
+        <TagContainer>
+          {Object.keys(definedTags).map((tag, index) => (
+            <Tag
+              key={index}
+              value={tag}
+              label={definedTags[tag].label}
+              icon={definedTags[tag].icon}
+              disabled={this.state.checkboxDisabled}
+              defaultChecked={selectedTags ? selectedTags.some(sTag => sTag === tag) : false}
+              addToSelectedTags={newTag => this.addToSelectedTags(newTag)}
+              removeFromSelectedTags={oldTag => this.removeFromSelectedTags(oldTag)}
+            />
+          ))}
+        </TagContainer>
+      </TagEditorContainer>
+    );
+  }
 }
 
 TagEditor.propTypes = {};
