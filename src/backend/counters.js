c0d3ad5602201a444adf2ef4115fd4fb70619bd6
Markus Kremer <mkremer@seibert-media.net>
2020-10-18 23:08:40 +0200

    rewrite counters

--- src/backend/counters.js
+++ src/backend/counters.js
@@ -1,22 +1,18 @@
-import { BACKEND_URL, getToken } from "./_main";
+import { BACKEND_URL } from "./_main";
 import axios from "axios";
 
 export const countAllEntries = () => {
-  return fetch(`${BACKEND_URL}/entries/count/`, {
-    method: "GET"
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  try {
+    return axios.get(`${BACKEND_URL}/entries/count/`);
+  } catch (error) {
+    return error.response;
+  }
 };
 
-export const countRecordsInRange = (start, end) => {
-  return fetch(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+export const countRecordsInRange = async (start, end) => {
+  try {
+    return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`)
+  } catch (error) {
+    return error.response
+  }
 };
