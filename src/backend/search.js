bc1153d3cddb6305a6e2488de65f4cee0a5abc2f
Markus Kremer <mkremer@seibert-media.net>
2020-10-18 23:24:41 +0200

    rewrite search

--- src/backend/search.js
+++ src/backend/search.js
@@ -2,13 +2,13 @@ import { BACKEND_URL, getToken } from "./_main";
 import axios from "axios";
 
 export const search = query => {
-  return fetch(`${BACKEND_URL}/entries/search?q=${query}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  try {
+    return axios.get(`${BACKEND_URL}/entries/search?q=${query}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
+  } catch (error) {
+    return error.response;
+  }
 };
