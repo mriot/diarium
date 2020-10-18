beb53f72de4973251cae255076489f106ab61041
Markus Kremer <mkremer@seibert-media.net>
2020-10-18 23:24:32 +0200

    add missing semis

--- src/backend/counters.js
+++ src/backend/counters.js
@@ -11,8 +11,8 @@ export const countAllEntries = () => {
 
 export const countRecordsInRange = async (start, end) => {
   try {
-    return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`)
+    return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`);
   } catch (error) {
-    return error.response
+    return error.response;
   }
 };
