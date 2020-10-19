ca8792d12da292f049e89522727153168a739fde
Markus Kremer <mkremer@seibert-media.net>
2020-10-19 23:16:12 +0200

    fix & rewrite meta-data component

--- src/components/sidebar/meta-data.jsx
+++ src/components/sidebar/meta-data.jsx
@@ -2,59 +2,56 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import moment from "moment";
-import { GlobalContext } from "../../contexts";
 import { WHITE } from "../../themes/diarium-theme";
+import { useRecoilValue } from "recoil";
+import { dayRecordAtom } from "../../atoms";
 
-const StyledMetaData = styled.div `
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	flex: 1;
-	font-size: 14px;
-	padding: 5px;
-	border-bottom: 1px solid #191919;
-	box-shadow: 0px 2px 3px 0 rgba(0, 0, 0, 0.2);
+const StyledMetaData = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  flex: 1;
+  font-size: 14px;
+  padding: 5px;
+  border-bottom: 1px solid #191919;
+  box-shadow: 0px 2px 3px 0 rgba(0, 0, 0, 0.2);
 `;
-const MetaInfoBlock = styled.div `
-	color: ${WHITE};
-	padding: 5px 0;
+const MetaInfoBlock = styled.div`
+  color: ${WHITE};
+  padding: 5px 0;
 
-	span {
-		opacity: 0.5;
-		display: block;
-	}
+  span {
+    opacity: 0.5;
+    display: block;
+  }
 `;
 
-export default class MetaData extends React.PureComponent {
-	render() {
-		const { GLOBAL_DAYRECORD } = this.context;
+export default function MetaData() {
+  const dayRecord = useRecoilValue(dayRecordAtom);
 
-		return (
-			<StyledMetaData>
-				<MetaInfoBlock>
-					Zuletzt bearbeitet:
-					<span>
-						{GLOBAL_DAYRECORD ? `${moment(GLOBAL_DAYRECORD.updatedAt).format("dd, D.MM.YYYY — HH:mm:ss")} Uhr` : "n/a"}
-					</span>
-				</MetaInfoBlock>
+  return (
+    <StyledMetaData>
+      <MetaInfoBlock>
+          Zuletzt bearbeitet:
+        <span>
+          {dayRecord ? `${moment(dayRecord.updated_at).format("dd, D.MM.YYYY — HH:mm:ss")} Uhr` : "n/a"}
+        </span>
+      </MetaInfoBlock>
 
-				<MetaInfoBlock>
-					Erstellt:
-					<span>
-						{GLOBAL_DAYRECORD ? `${moment(GLOBAL_DAYRECORD.createdAt).format("dd, D.MM.YYYY — HH:mm:ss")} Uhr` : "n/a"}
-					</span>
-				</MetaInfoBlock>
-			</StyledMetaData>
-		);
-	}
+      <MetaInfoBlock>
+          Erstellt:
+        <span>
+          {dayRecord ? `${moment(dayRecord.created_at).format("dd, D.MM.YYYY — HH:mm:ss")} Uhr` : "n/a"}
+        </span>
+      </MetaInfoBlock>
+    </StyledMetaData>
+  );
 }
 
 MetaData.propTypes = {
-	
+
 };
 
 MetaData.defaultProps = {
-	
-};
 
-MetaData.contextType = GlobalContext;
+};
