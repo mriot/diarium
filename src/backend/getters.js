7e723089b7421dd7baa40707ac9f2774fb1ade95
Markus Kremer <mkremer@seibert-media.net>
2020-10-19 18:20:07 +0200

    axiosify

--- src/backend/getters.js
+++ src/backend/getters.js
@@ -4,49 +4,49 @@ import axios from "axios";
 export const getRecordsInRange = (start, end, columns) => {
   const params = (columns.map(col => `column=${col}`).join("&"));
 
-  return fetch(`${BACKEND_URL}/entries/range/?start=${start}&end=${end}&${params}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  try {
+    return axios.get(`${BACKEND_URL}/entries/range/?start=${start}&end=${end}&${params}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
+  } catch (error) {
+    return error.response;
+  }
 };
 
 export const getAllEntriesForYear = year => {
-  return fetch(`${BACKEND_URL}/entries/${year}/`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  try {
+    return axios.get(`${BACKEND_URL}/entries/${year}/`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
+  } catch (error) {
+    return error.response;
+  }
 };
 
 export const getAllEntriesForYearMonth = (year, month) => {
-  return fetch(`${BACKEND_URL}/entries/${year}/${month}/`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  try {
+    return axios.get(`${BACKEND_URL}/entries/${year}/${month}/`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
+  } catch (error) {
+    return error.response;
+  }
 };
 
 export const getRecordForDay = (year, month, day) => {
-  return fetch(`${BACKEND_URL}/entries/${year}/${month}/${day}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    }
-  })
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  try {
+    return axios.get(`${BACKEND_URL}/entries/${year}/${month}/${day}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
+  } catch (error) {
+    return error.response;
+  }
 };
