141b96ae6ecfa56443facc09c860ac614df9ef2d
Markus Kremer <mkremer@seibert-media.net>
2020-10-20 22:01:54 +0200

    add required auth header

--- src/backend/counters.js
+++ src/backend/counters.js
@@ -1,4 +1,4 @@
-import { BACKEND_URL } from "./_main";
+import { BACKEND_URL, getToken } from "./_main";
 import axios from "axios";
 
 export const countAllEntries = () => {
@@ -11,7 +11,11 @@ export const countAllEntries = () => {
 
 export const countRecordsInRange = async (start, end) => {
   try {
-    return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`);
+    return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    });
   } catch (error) {
     return error.response;
   }
