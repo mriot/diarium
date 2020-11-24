0b808132a536ae4a8d26a48552b89d36c6b64829
Markus Kremer <mkremer@seibert-media.net>
2020-11-24 12:00:18 +0100

    convert to class

--- src/components/editor/AutoSave.js
+++ src/components/editor/AutoSave.js
@@ -1,46 +1,48 @@
-export default function AutoSave(editorState, saveCallback) {
-  let timer = null;
-  let internalTimer = null;
+export default class AutoSave {
+  constructor(editorState, saveCallback) {
+    let timer = null;
+    let internalTimer = null;
 
-  const save = () => {
-    console.log(editorState);
-    if (!editorState) console.error("No editorState provided");
-    if (!editorState?.isDirty()) return false;
+    const save = () => {
+      console.log(editorState);
+      if (!editorState) { console.error("No editorState provided"); }
+      if (!editorState?.isDirty()) { return false; }
 
-    try {
-      console.log("Saving...");
-      saveCallback(editorState.getContent());
-      // todo: check if request was successful first
-      editorState.setDirty(false);
-    } catch (error) {
-      console.error("Error while trying to save content", error);
-    }
-  };
+      try {
+        console.log("Saving...");
+        saveCallback(editorState.getContent());
+        // todo: check if request was successful first
+        editorState.setDirty(false);
+      } catch (error) {
+        console.error("Error while trying to save content", error);
+      }
+    };
 
-  const setInternalTimer = () => {
-    internalTimer = setTimeout(() => {
-      internalTimer = null;
-      save();
-    }, 6000);
-  };
+    const setInternalTimer = () => {
+      internalTimer = setTimeout(() => {
+        internalTimer = null;
+        save();
+      }, 6000);
+    };
 
-  // replace a previously scheduled save with each invocation
-  this.start = function (timeout = 3000) {
-    this.clear();
+    // replace a previously scheduled save with each invocation
+    this.start = function (timeout = 3000) {
+      this.clear();
 
-    timer = setTimeout(() => {
-      this.clear(true);
-      save();
-    }, timeout);
+      timer = setTimeout(() => {
+        this.clear(true);
+        save();
+      }, timeout);
 
-    if (!internalTimer) setInternalTimer();
-  };
+      if (!internalTimer) { setInternalTimer(); }
+    };
 
-  this.clear = function (all) {
-    clearTimeout(timer);
-    if (all) {
-      clearTimeout(internalTimer);
-      internalTimer = null;
-    }
-  };
+    this.clear = function (all) {
+      clearTimeout(timer);
+      if (all) {
+        clearTimeout(internalTimer);
+        internalTimer = null;
+      }
+    };
+  }
 }
