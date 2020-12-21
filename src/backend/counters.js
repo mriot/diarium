348027d9d117a15f19fe21e559e37e7eec4cbc8c
Markus Kremer <mkremer@seibert-media.net>
2020-12-21 01:18:58 +0100

    fix error returns

--- src/backend/counters.js
+++ src/backend/counters.js
@@ -2,21 +2,25 @@ import { BACKEND_URL, getToken } from "./_main";
 import axios from "axios";
 
 export const countAllEntries = () => {
-  try {
-    return axios.get(`${BACKEND_URL}/entries/count/`);
-  } catch (error) {
-    return error.response;
-  }
+  return axios.get(`${BACKEND_URL}/entries/count/`, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  }).catch(error => error.response);
+};
+
+export const count = (path = "") => {
+  return axios.get(`${BACKEND_URL}/entries/count/${path}`, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  }).catch(error => error.response);
 };
 
 export const countRecordsInRange = async (start, end) => {
-  try {
-    return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`, {
-      headers: {
-        Authorization: `Bearer ${getToken()}`
-      }
-    });
-  } catch (error) {
-    return error.response;
-  }
+  return axios.get(`${BACKEND_URL}/entries/range/count/?start=${start}&end=${end}`, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  }).catch(error => error.response);
 };
