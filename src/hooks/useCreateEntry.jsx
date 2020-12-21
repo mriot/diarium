bcc486617151f599821db7f6d135ccd519186018
Markus Kremer <mkremer@seibert-media.net>
2020-12-21 01:53:57 +0100

    remove doubled dayjs()

--- src/hooks/useCreateEntry.jsx
+++ src/hooks/useCreateEntry.jsx
@@ -1,5 +1,5 @@
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { dayRecordAtom, readModeAtom, selectedDayAtom } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { dayRecordAtom, readModeAtom } from "../atoms";
 import { createNewEntry } from "../backend/recordManipulation";
 import dayjs from "dayjs";
 import { useLocation } from "react-router-dom";
@@ -14,8 +14,8 @@ export default function useCreateEntry() {
 
     try {
       const response = await createNewEntry({
-        assigned_day: dayjs(selectedDay).format("YYYY-MM-DD"),
-        content: `<h1>${dayjs(selectedDay).format("dddd, D. MMMM YYYY")}</h1><hr><p></p>`,
+        assigned_day: selectedDay.format("YYYY-MM-DD"),
+        content: `<h1>${selectedDay.format("dddd, D. MMMM YYYY")}</h1><hr><p></p>`,
         tags: []
       });
 
