0a7edbd69c1e674309ec8791131911fc6cabaf29
Markus Kremer <mkremer@seibert-media.net>
2020-12-21 11:24:47 +0100

    (wip) add useSelectedDay hook

---
+++ src/hooks/useSelectedDay.jsx
@@ -0,0 +1,19 @@
+import { useRef } from "react";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { useLocation, useHistory } from "react-router-dom";
+
+dayjs.extend(customParseFormat);
+
+export default function useSelectedDay() {
+  const location = useLocation();
+  const history = useHistory();
+  let selectedDay = dayjs(location.pathname, "/YYYY/MM/DD", true);
+
+  if (!selectedDay.isValid()) {
+    selectedDay = dayjs(); // if invalid, use today's date
+    history.replace(selectedDay.format("/YYYY/MM/DD"));
+  }
+
+  return useRef(selectedDay.format("/YYYY/MM/DD"));
+}
